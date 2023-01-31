a92d4f7b577f7b456f94513639a52259c59459a7
Leonardo Barros <leonprogramer@gmail.com>
2023-01-31 17:16:06 -0300

    Test with data sources
    Adding data sources configurations to use one cloud database.

---
+++ server/src/database/data-source-firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+	apiKey: "AIzaSyBLhCEuZI9KPyHq3aJqKfnPzvFy35QIL8M",
+	authDomain: "db-agrodigital.firebaseapp.com",
+	databaseURL: "https://db-agrodigital-default-rtdb.firebaseio.com",
+	projectId: "db-agrodigital",
+	storageBucket: "db-agrodigital.appspot.com",
+	messagingSenderId: "1029886301674",
+	appId: "1:1029886301674:web:70c1538df6127924788e93"
+};
